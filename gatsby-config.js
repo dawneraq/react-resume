94c762eddbfd9ac9931077cd57bc4666f93daa2c
dawneraq <dawneraq@gmail.com>
2020-07-24 19:45:12 -0700

    add basic info to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Andrew Aquino`,
+    author: `Andrew Aquino`,
+    description: `I'm a frontend developer in Southern California. These are my projects and notes about JavaScript, CSS, design, and anything else I'm learning.`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/aqandrew`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/dawneraq`,
       },
     ],
   },
